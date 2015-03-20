f52971d64b78cca4cf6c1622392e4b1076b7196c
Michel Hong <yanshu.hong.michael@gmail.com>
2015-03-20 15:30:00 -0700

    Initial Commit

---
+++ MHKit/MHKit.m
@@ -0,0 +1,13 @@
+//
+//  MHKit.m
+//  MHKit
+//
+//  Created by Michael Hong on 3/20/15.
+//  Copyright (c) 2015 Michael Hong. All rights reserved.
+//
+
+#import "MHKit.h"
+
+@implementation MHKit
+
+@end
